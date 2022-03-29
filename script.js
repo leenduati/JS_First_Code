9504507d6db62612c991934762a0b84ad19d0724
leenduati <leenduati21@gmail.com>
2022-03-29 20:46:25 +0300

    Added script.js file

---
+++ script.js
@@ -0,0 +1,6 @@
+var input = parseInt(prompt("Please enter value in pounds: "))
+var output = input * 0.454
+    // Concatenate strings in JS
+alert("Your weight is " + Math.round(output, 2) + " KGs")
+    // Console part
+console.log("Conversion Completed")
