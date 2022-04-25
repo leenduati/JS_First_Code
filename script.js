c5f4dc86d6ff95ba17ef3dc452e23c40529cf3ac
leenduati <leenduati21@gmail.com>
2022-04-25 14:57:09 +0300

    Finished and wrote code for Part * JS Exercises

--- script.js
+++ script.js
@@ -47,3 +47,64 @@ while (result <= 10) {
     result += 1
 }
 //for loops
+
+/// PART 8 - LOOP EXERCISES
+// Before we continue on with your project, let's practice some loops!
+// You'll have just two problems, but you'll need to use each loop type we
+// learned about to solve them!
+
+
+///////////////////
+//// PROBLEM 1 ///
+/////////////////
+
+// Use a For Loop to print (console.log()) out the word "hello" 5 times.
+//
+// Do this with a While Loop and a For Loop
+
+// While Loop
+
+var num = 0;
+while (num < 5) {
+    console.log("Hello using a while loop");
+    num++;
+}
+
+
+
+// For Loop
+
+for (let numFor = 0; numFor < 5; numFor++) {
+    console.log("Hello using a for loop");
+
+}
+
+
+
+
+/////////////////
+// PROBLEM 2 ///
+///////////////
+
+// Use Loops to console.log() (print out) all the odd numbers from 1 to 25
+// Do this using two methods, a while loop and a for loop
+
+// METHOD ONE
+// While Loop
+
+numWhile = 0
+while (numWhile < 26) {
+    if (numWhile % 2 != 0) {
+        console.log("Odd numbers using while " + numWhile);
+    }
+    numWhile++;
+}
+
+// METHOD TWO
+// For Loop
+for (let index = 0; index < 26; index++) {
+    if (index % 2 != 0) {
+        console.log("Odd numbers using for " + index);
+    }
+
+}
