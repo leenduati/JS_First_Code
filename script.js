33e3ab411b0119a58443b88e2394bc3110bc0cd1
leenduati <leenduati21@gmail.com>
2022-04-30 00:15:03 +0300

    Added the Objects Exercise Solutions

--- script.js
+++ script.js
@@ -500,3 +500,66 @@ var objName = {
         }
     }
     // Without the this keyword, the name referenced above produces a deprecated error,meaning it's void
+
+// JS Part Exercise
+// Part 6 - Objects Exercise
+
+////////////////////
+// PROBLEM 1 //////
+//////////////////
+
+// Given the object:
+var employee = {
+    name: "John Smith",
+    job: "Programmer",
+    age: 31,
+    nameLength: function() {
+        console.log(this.employee["name"].length)
+            //this.name.length
+    }
+}
+
+// Add a method called nameLength that prints out the
+// length of the employees name to the console.
+
+
+///////////////////
+// PROBLEM 2 /////
+/////////////////
+
+// Given the object:
+var employee = {
+    name: "John Smith",
+    job: "Programmer",
+    age: 31,
+    alFN: function() {
+        console.log("Name is " + this.name + ", Job is " + this.job + " Age is " + this.age)
+    }
+}
+
+// Write program that will create an Alert in the browser of each of the
+// object's values for the key value pairs. For example, it should alert:
+
+// Name is John Smith, Job is Programmer, Age is 31.
+employee.alFN(0)
+
+
+///////////////////
+// PROBLEM 3 /////
+/////////////////
+
+// Given the object:
+var employee = {
+    name: "John Smith",
+    job: "Programmer",
+    age: 31,
+    lastNameCall: function() {
+        return this.name.split(" ")[1]
+    }
+}
+
+// Add a method called lastName that prints
+// out the employee's last name to the console.
+
+// You will need to figure out how to split a string to an array.
+// Hint: http://www.w3schools.com/jsref/jsref_split.asp
